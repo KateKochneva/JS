dd7e6f278bc37cf6640d4fa52f42748dfac55153
Kate Kochneva <kate-kochneva@mail.ru>
2020-11-02 21:48:31 +0300

    Домашнее 3

---
+++ 3-1.js
@@ -0,0 +1,23 @@
+/*1. С помощью цикла while вывести все простые числа в промежутке от 0 до 100.*/
+
+    let prime = 1;
+    let primeArr = [];
+
+    while (prime <= 100) {
+        if (prime == 1) {
+            primeArr.push(1)
+        } else {
+            let result = 2;
+            let i = 2;
+            while (prime % i !== 0 ){
+                i += 1;
+                result +=1;
+                } 
+            if (result == prime) {
+                primeArr.push(result);
+            }
+        }
+        prime += 1;
+    }
+
+    console.log(primeArr);
